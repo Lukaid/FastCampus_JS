887d89c88a68670151bb9cab63ee75ac2a17dff5
Lukaid <crescent3859@gmail.com>
2021-04-06 04:23:39 +0900

    0406

--- js_pre/main.js
+++ js_pre/main.js
@@ -1,1 +1,19 @@
-console.log('Lukaid');
+// let myName = "Lukaid";
+// let email = 'crescent3859@gmail.com'
+// let hello = `Hello ${myName}?!`
+
+// console.log(myName);
+// console.log(email);
+// console.log(hello);
+
+
+let user = {
+  name: "Lukaid",
+  age: 28,
+
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.name);
+
