af8df8c6ae076dc8525e171376e32c615070e52a
Lukaid <crescent3859@gmail.com>
2021-06-16 03:33:40 +0900

    0616

--- js_test/dist/main.1f19ae8e.js
+++ js_test/dist/main.1f19ae8e.js
@@ -312,6 +312,15 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // timeout(()=>{
 //   console.log('Done!')
 // })
+// 생성자 함수
+var lukaid = {
+  firstName: 'SeongWoo',
+  lastName: 'Lee',
+  getFullName: function getFullName() {
+    return "".concat(this.firstName, " ").concat(this.lastName);
+  }
+};
+console.log(lukaid);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -340,7 +349,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54326" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50763" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
