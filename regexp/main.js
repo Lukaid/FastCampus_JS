d492bdad1ceed5500393717866376e041290bd3f
Lukaid <crescent3859@gmail.com>
2021-07-22 03:27:46 +0900

    0722

--- regexp/main.js
+++ regexp/main.js
@@ -2,10 +2,26 @@ const str = `
 010-1234-5678
 thesecon@gmail.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
-The quick brown fox jumps over the lazy dog
+The quick brown fox jumps over the lazy dog.
 abbcccdddd
+http://localhost:1234
 `
 
 // const regexp = new RegExp('the', 'gi')
-const regexp = /the/gi
-console.log(str.match(regexp))
+// const regexp = /fox/gi
+// console.log(str.match(regexp))
+// console.log(regexp.test(str))
+// console.log(str.replace(regexp, 'AAA'))
+// console.log(str)
+
+// console.log(str.match(/the/gi))
+
+const h = `  the hello  world   !`
+
+console.log(str.match(/\bf\w{1,}\b/gmi))
+
+console.log(h.replace(/\s/gmi, ""))
+
+
+console.log(str.match(/.{1,}(?=@)/g))
+console.log(str.match(/(?<=@).{1,}/g))
