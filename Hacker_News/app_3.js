f2084b637cc3750f7244074823000df3a0b443d0
Lukaid <crescent3859@gmail.com>
2021-08-06 05:00:33 +0900

    ts start

--- Hacker_News/app_3.js
+++ Hacker_News/app_3.js
@@ -1,3 +1,5 @@
+// 페이징 구현하기
+
 const container = document.getElementById('root')
 const ajax = new XMLHttpRequest()
 const content = document.createElement('div')
@@ -18,12 +20,13 @@ function getData (url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL)
   const newsList = []
+  console.log(newsFeed)
 
   newsList.push('<ul>')
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage-1)*10; i < store.currentPage*10; i++) {
     newsList.push(`
       <li>
-        <a href = "#${newsFeed[i].id}">
+        <a href = "#/show/${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
@@ -33,17 +36,17 @@ function newsFeed() {
   
   newsList.push(`
     <div>
-      <a href="#/${store.currentPage - 1}">이전 페이지</a>
-      <a href="#/${store.currentPage + 1}">다음 페이지</a>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
     </div>
   `)
 
-  container.innerHTML = newsList.join('')
+  container.innerHTML = newsList.join('') // overwrite
 }
 
 // 클릭시 (해쉬가 바뀔 경우)보여줄 화면, index.html에 선언된 div태그인 container의 내용을 바꿈 
 function newsDetail() {
-  const id = location.hash.substr(1)
+  const id = location.hash.substr(7)
   const newsContent = getData(CONTENT_URL.replace('@id', id))
   // const title = document.createElement('h1')
 
@@ -51,16 +54,19 @@ function newsDetail() {
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `
 }
 
 // 사용자가 선택한 해쉬에 따라 보여주는 화면을 전환하는 함수
 function router() {
-  const routePath = location.hash // #은 안읽음
+  const routePath = location.hash //location.hash에 #만 들어가 있으면 빈 문자열을 반환함.
 
   if (routePath === '') {
+    newsFeed()
+  } else if (routePath.indexOf('#/page/') >= 0){
+    store.currentPage = Number(routePath.substr(7))
     newsFeed()
   } else {
     newsDetail()
---
+++ Hacker_News/app_4.js
@@ -0,0 +1,85 @@
+// 템플릿 구현
+
+const container = document.getElementById('root')
+const ajax = new XMLHttpRequest()
+const content = document.createElement('div')
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+const store = {
+  currentPage: 1,
+}
+
+// ajax를 이용하여 url의 데이터 가져오기
+function getData (url) {
+  ajax.open('GET', url, false)
+  ajax.send()
+  return JSON.parse(ajax.response)
+}
+
+// newsFeed를 만드는 함수, html 구조를 만듬, array로 만들어서 마지막에 innerHTML으로 array의 elements를 join하여 넣음
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL)
+  const newsList = []
+  let template = `
+  <div class="container mx-auto p-4">
+    <h1>Hacker News</h1>
+    <ul>
+      {{__news_feed__}}
+    </ul>
+    <div>
+      <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+      <a href="#/page/{{__next_page__}}">다음 페이지</a>
+    </div>
+  </div>
+  `
+
+  for (let i = (store.currentPage-1)*10; i < store.currentPage*10; i++) {
+    newsList.push(`
+      <li>
+        <a href = "#/show/${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `)
+  }
+
+  template = template.replace('{{__news_feed__}}', newsList.join(''))
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1)
+  template = template.replace('{{__next_page__}}', store.currentPage + 1)
+
+  container.innerHTML = template
+}
+
+// 클릭시 (해쉬가 바뀔 경우)보여줄 화면, index.html에 선언된 div태그인 container의 내용을 바꿈 
+function newsDetail() {
+  const id = location.hash.substr(7)
+  const newsContent = getData(CONTENT_URL.replace('@id', id))
+  // const title = document.createElement('h1')
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#/page/${store.currentPage}">목록으로</a>
+    </div>
+  `
+}
+
+// 사용자가 선택한 해쉬에 따라 보여주는 화면을 전환하는 함수
+function router() {
+  const routePath = location.hash //location.hash에 #만 들어가 있으면 빈 문자열을 반환함.
+
+  if (routePath === '') {
+    newsFeed()
+  } else if (routePath.indexOf('#/page/') >= 0){
+    store.currentPage = Number(routePath.substr(7))
+    newsFeed()
+  } else {
+    newsDetail()
+  }
+}
+
+// 브라우저에서 event가 일어나면 router를 실행
+window.addEventListener('hashchange', router)
+
+router()
